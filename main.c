8dc4e26fea1deec947f0719eff3f9742dc979e3f
Liam-Sango <anonymous_aussie42@proton.me>
2025-11-12 10:53:37 +1100

    Added move logic to the main loop for both players.

--- main.c
+++ main.c
@@ -15,6 +15,66 @@ char gameboard[3][3] =
 
 /// Main function
 int main () {
+    //Variables
+    bool player_1_Active;
+    bool player_2_Active;
+    int move_target[1];
+    int player_number;
+
+    //Greets the players.
+    printf("\nWelcome to Norts and Crosses.\n\n");
+
+
+    //Main_Loop
+    while (true) {
+    }
+
+    //PLAYER 1
+    if (player_1_Active == true) {
+    move_target[0] = 0, move_target[1] = 0;
+    player_number = 1;
+    //Gathers the row where player 1 wishes to place a peice.
+    printf("\n\n##Player 1##\n\nPlease enter The row number of the space where you want to place your piece.\n");
+    scanf("%d", &move_target[0]);
+    if (move_target[0] > 3 || move_target[0] < 1) {
+        printf("\nYou entered an invalid Space... Exiting\n");
+        exit(0); }
+    
+    //Gathers the collum where player 1 wishes to place a peice.
+    printf("\n\n##Player 1##\n\nPlease enter The Collum number of the space where you want to place your piece.\n");
+    scanf("%d", &move_target[1]);
+    if (move_target[1] > 3 || move_target[1] < 1) {
+        printf("\nYou entered an invalid Space... Exiting\n");
+        exit(0); }
+
+    check_board_fill(gameboard);
+    make_game_move(gameboard,move_target, player_number);
+    }
+
+    //PLAYER 2
+    
+    if (player_2_Active == true) {
+    move_target[0] = 0, move_target[1] = 0;
+    player_number = 2;
+    //Gathers the row where player 2 wishes to place a peice.
+    printf("\n\nPlayer 2\nPlease enter The row number of the space where you want to place your piece.\n");
+    scanf("%d", &move_target[0]);
+    if (move_target[0] > 3 || move_target[0] < 1) {
+        printf("\nYou entered an invalid Space... Exiting\n");
+        exit(0); }
+    
+    //Gathers the collum where player 2 wishes to place a peice.
+    printf("\n\nPlayer 2\nPlease enter The Collum number of the space where you want to place your piece.\n");
+    scanf("%d", &move_target[1]);
+    if (move_target[1] > 3 || move_target[1] < 1) {
+        printf("\nYou entered an invalid Space... Exiting\n");
+        exit(0); }
+    
+    check_board_fill(gameboard);
+    make_game_move(gameboard,move_target, player_number);
+    }
+    
+
     return 0;
 }
 
@@ -69,6 +129,7 @@ void make_game_move(char game_board[3][3], int target[2], int player_number) {
         game_board[row][col] = player_symbol;
     } else {
         printf("Invalid move! Cell already occupied.\n");
+        exit(0);
     }
 }
 
