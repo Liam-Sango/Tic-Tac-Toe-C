1351f7fc8dfe5d73fd40273b9746e52893676b16
Liam-Sango <anonymous_aussie42@proton.me>
2025-11-11 11:23:56 +1100

    Rewrote both diagonal win detection code blocks, completing them.

--- Testing/Function_test.c
+++ Testing/Function_test.c
@@ -4,9 +4,9 @@
 
 int Win_status = 0;
 char gameboard[3][3] = 
-{ {'O', 'O', 'O'}, 
-  {' ', 'X', ' '}, 
-  {' ', ' ', 'O'} };
+{ {'O', ' ', 'X'}, 
+  {' ', 'O', ' '}, 
+  {'X', ' ', 'O'} };
 
 int check_winner (char game_board[3][3]) {
 char Player1_symbol = 'X', Player2_symbol = 'O';
@@ -53,22 +53,39 @@ for (c = 0; c < 3; c++) {
 int player1_count = 0, player2_count = 0;
 //Check for wins on the primary diagonal
 for (c = 0; c < 3; c++) {
-    for (r = 0; r < 3; r++) {
-        if (game_board[c] == game_board[r] && game_board[c][r] == Player1_symbol) {
-            player1_count++;
-            if (player1_count >= 3) {
-                Win_status = 1; // Player 1 wins
-                return Win_status;
-             }
-        } else if (game_board[c] == game_board[r] && game_board[c][r] == Player2_symbol) {
-            player2_count++;
-            if (player2_count >= 3) {
-                Win_status = 2; // Player 2 wins
-                return Win_status;
-            }
-        }
-    }
+  r = c;
+  if (game_board[c][r] == Player1_symbol) {
+    player1_count++;
+  } else if ( game_board [c][r] == Player2_symbol) {
+    player2_count++;
+  }
 }
+if (player1_count == 3) {
+    Win_status = 1; // Player 1 wins
+    return Win_status;
+} else if (player2_count == 3) {
+    Win_status = 2; // Player 2 wins
+    return Win_status;
+}
+
+//Check for wins on the Secondary diagonal
+player1_count = 0, player2_count = 0;
+for (c = 0; c < 3; c++) {
+    r = 3 - c - 1;
+    if (game_board[c][r] == Player1_symbol) {
+    player1_count++;
+  } else if ( game_board [c][r] == Player2_symbol) {
+    player2_count++;
+  }
+}
+if (player1_count == 3) {
+    Win_status = 1; // Player 1 wins
+    return Win_status;
+} else if (player2_count == 3) {
+    Win_status = 2; // Player 2 wins
+    return Win_status;
+}
+
 return Win_status;
 }
 
